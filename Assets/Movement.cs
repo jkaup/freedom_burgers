3884b220a27d83d4caa243c2cd2baedfd1062141
jj <noname@nomail.fi>
2025-06-06 19:58:09 +0300

    Add scene, movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal input
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1f); // Optional: prevents faster diagonal movement
+
+        Rigidbody body = gameObject.GetComponent<Rigidbody>();
+        body.AddForce(move * 1f);
+    }
+}
